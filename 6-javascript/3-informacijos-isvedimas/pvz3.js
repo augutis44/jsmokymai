6c774179e30dfeb2e644a8f1789b4d2493a6c234
augutis44 <kraujalytee@gmail.com>
2023-11-22 23:28:56 +0200

    projketas su SASS bei kiti uzsilike dalykai

---
+++ 6-javascript/3-informacijos-isvedimas/pvz3.js
@@ -0,0 +1,6 @@
+console.log('pirma dalis' + ' antra dalis')
+console.log('==============================')
+console.log('pirma ' + 'antra' + ' trecia')
+console.log('pirma' , 'antra' , 'trecia')
+console.log('pirma' + 'antra' , 'trecia')
+console.log('==============================')
---
+++ 6-javascript/3-informacijos-isvedimas/pvz6.js
@@ -0,0 +1,5 @@
+alert('veikia');
+
+document.getElementById('tekstas1').innerHTML = 'naujas paragrafo tekstas';
+document.getElementById('tekstas2').innerHTML = 'naujas kito tekstas';
+document.getElementById('tekstas3').innerHTML += ' toliau tesiasi sio elemento tekstas';
