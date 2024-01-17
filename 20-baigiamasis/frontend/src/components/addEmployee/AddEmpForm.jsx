75a048f5f1f60f110f7b73902180520966f0683a
augutis44 <kraujalytee@gmail.com>
2024-01-17 21:10:44 +0200

    input change

--- 20-baigiamasis/frontend/src/components/addEmployee/AddEmpForm.jsx
+++ 20-baigiamasis/frontend/src/components/addEmployee/AddEmpForm.jsx
@@ -49,7 +49,7 @@ const AddEmpForm = () => {
                 <input
                     type="text"
                     id="firstNameInput"
-                    className="border w-[450px] box-border p-2 rounded-sm"
+                    className="border-2 border-slate-300 w-[450px] box-border p-2 rounded-sm"
                     onChange={(event) => setFirstName(event.target.value)}
                 />
             </div>
@@ -59,7 +59,7 @@ const AddEmpForm = () => {
                 <input
                     type="text"
                     id="lastNameInput"
-                    className="border w-[450px] box-border p-2 rounded-sm"
+                    className="border-2 border-slate-300 w-[450px] box-border p-2 rounded-sm"
                     onChange={(event) => setLastName(event.target.value)}
                 />
             </div>
@@ -69,7 +69,7 @@ const AddEmpForm = () => {
                 <input
                     type="text"
                     id="emailInput"
-                    className="border w-[450px] box-border p-2 rounded-sm"
+                    className="border-2 border-slate-300 w-[450px] box-border p-2 rounded-sm"
                     onChange={(event) => setEmail(event.target.value)}
                 />
             </div>
@@ -79,7 +79,7 @@ const AddEmpForm = () => {
                 <input
                     type="text"
                     id="addressInput"
-                    className="border w-[450px] box-border p-2 rounded-sm"
+                    className="border-2 border-slate-300 w-[450px] box-border p-2 rounded-sm"
                     onChange={(event) => setAddress(event.target.value)}
                 />
             </div>
@@ -89,7 +89,7 @@ const AddEmpForm = () => {
                 <input
                     type="number"
                     id="phoneNumberInput"
-                    className="border w-[450px] box-border p-2 rounded-sm"
+                    className="border-2 border-slate-300 w-[450px] box-border p-2 rounded-sm"
                     onChange={(event) => setPhone(event.target.value)}
                 />
             </div>
@@ -99,7 +99,7 @@ const AddEmpForm = () => {
                 <input
                     type="text"
                     id="departamentInput"
-                    className="border w-[450px] box-border p-2 rounded-sm"
+                    className="border-2 border-slate-300 w-[450px] box-border p-2 rounded-sm"
                     onChange={(event) => setDepartament(event.target.value)}
                 />
             </div>
@@ -109,7 +109,7 @@ const AddEmpForm = () => {
                 <input
                     type="text"
                     id="locationInput"
-                    className="border w-[450px] box-border p-2 rounded-sm"
+                    className="border-2 border-slate-300 w-[450px] box-border p-2 rounded-sm"
                     onChange={(event) => setLocation(event.target.value)}
                 />
             </div>
@@ -125,7 +125,7 @@ const AddEmpForm = () => {
             </div>
 
             <div className="flex justify-end">
-                <button type="submit" className="bg-green-400 py-2 px-4 border rounded-md"> SUBMIT </button>
+                <button type="submit" className="bg-green-500 py-2 px-4 border rounded-md text-white"> SUBMIT </button>
             </div>
         </form>
     )
