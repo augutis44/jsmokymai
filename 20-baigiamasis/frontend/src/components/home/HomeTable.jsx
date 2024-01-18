59a6520a891f28fbc9e38ea24bcdc34d4232eb66
augutis44 <kraujalytee@gmail.com>
2024-01-18 14:15:21 +0200

    home table style

--- 20-baigiamasis/frontend/src/components/home/HomeTable.jsx
+++ 20-baigiamasis/frontend/src/components/home/HomeTable.jsx
@@ -5,14 +5,16 @@ const HomeTable = ({ employees, fetchEmployees }) => {
 
     return (
 
-        <div className="my-12 border-solid border-2 m-auto w-[1200px] shadow-lg shadow-purple-100 font-font-regular">
-            <div className="bg-purple-600 p-5 text-white text-xl">
-                <p>Manage Employees</p>
+        <div className="my-12 border-solid border-2 m-auto w-[1200px] shadow-lg shadow-purple-100 font-font-regular rounded-md overflow-hidden">
+            <div className="bg-purple-600 p-5  flex justify-between items-center">
+                <p className="text-white text-2xl">Manage Employees</p>
+
+                <button className="bg-green-400 hover:bg-green-500 py-2 px-4 border-2 border-green-600 rounded-md text-lg">Add Employee</button>
             </div>
             <div className="p-5">
                 <table className="border w-full table-auto">
                     <thead>
-                        <tr className="text-left border-b-4">
+                        <tr className="text-left border-b-4 bg-gray-100">
                             <th className="py-4 pl-4">First Name</th>
                             <th className="py-4">Last Name</th>
                             <th className="py-4">Email</th>
