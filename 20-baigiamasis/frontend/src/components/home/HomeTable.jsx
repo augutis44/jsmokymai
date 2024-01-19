651e17575cde2c6cce23a8ab764d5b752894e154
augutis44 <kraujalytee@gmail.com>
2024-01-19 13:36:49 +0200

    table scroll

--- 20-baigiamasis/frontend/src/components/home/HomeTable.jsx
+++ 20-baigiamasis/frontend/src/components/home/HomeTable.jsx
@@ -2,7 +2,6 @@ import { NavLink } from "react-router-dom";
 import { FaCirclePlus } from "react-icons/fa6";
 import TableContent from "./TableContent";
 import SearchBar from "../searchBar/SearchBar";
-import { useEffect, useState } from "react";
 import TableHeaders from "./TableHeaders"
 import TableHeadresSorting from "./TableHeadresSorting"
 
@@ -29,36 +28,36 @@ const HomeTable = ({
                     </div>
                 </NavLink>
             </div>
-            <div className="p-5 h-[75vh] overflow-y-scroll">
-                <table className="border w-full table-auto">
-                    <thead>
-                        <tr className="text-left border-b-4 bg-gray-100">
-                            {/* sukurti kiekvienam th komponenta su savo state order, kad paspaudus pasikeistu i 1 arba -1. pradzioj bus nieko
-                            reikia sitam komponente funkcijos, kuri refreintu kitu komponentu state (headeriu) */}
-                            <TableHeadresSorting styles="pl-4" setFilter={setFilter} filter={filter} name="firstName">
-                                First Name
-                            </TableHeadresSorting>
-                            <TableHeadresSorting setFilter={setFilter} filter={filter} name="lastName">
-                                Last Name
-                            </TableHeadresSorting>
-                            <TableHeadresSorting setFilter={setFilter} filter={filter} name="email">
-                                Email
-                            </TableHeadresSorting>
-                            <TableHeadresSorting setFilter={setFilter} filter={filter} name="departament">
-                                Departament
-                            </TableHeadresSorting>
-                            <TableHeadresSorting setFilter={setFilter} filter={filter} name="location">
-                                Location
-                            </TableHeadresSorting>
-                            <TableHeaders styles="pr-4">
-                                Actions
-                            </TableHeaders>
-                        </tr>
-                    </thead>
-                    <TableContent employees={employees} fetchEmployees={fetchEmployees} />
-                </table >
+            <div className="">
+                <div className="m-5 h-[75vh] overflow-y-auto">
+                    <table className="border w-full table-auto">
+                        <thead>
+                            <tr className="text-left">
+                                <TableHeadresSorting styles="pl-4" setFilter={setFilter} filter={filter} name="firstName">
+                                    First Name
+                                </TableHeadresSorting>
+                                <TableHeadresSorting setFilter={setFilter} filter={filter} name="lastName">
+                                    Last Name
+                                </TableHeadresSorting>
+                                <TableHeadresSorting setFilter={setFilter} filter={filter} name="email">
+                                    Email
+                                </TableHeadresSorting>
+                                <TableHeadresSorting setFilter={setFilter} filter={filter} name="departament">
+                                    Departament
+                                </TableHeadresSorting>
+                                <TableHeadresSorting setFilter={setFilter} filter={filter} name="location">
+                                    Location
+                                </TableHeadresSorting>
+                                <TableHeaders styles="pr-4">
+                                    Actions
+                                </TableHeaders>
+                            </tr>
+                        </thead>
+                        <TableContent employees={employees} fetchEmployees={fetchEmployees} />
+                    </table >
+                </div >
             </div >
-        </div >
+        </div>
     )
 }
 
